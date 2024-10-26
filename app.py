31abd0e53fa9694c88a9e7d29d01923d9d423591
benkoo1094 <benkoo1094@gmail.com>
2024-10-26 16:31:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # Import necessary libraries and modules
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from openai import OpenAI
 import pandas as pd
